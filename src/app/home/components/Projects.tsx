fa8202821672b201a986dbe9804b1b04d33f3ee2
nurcholis_nutech <nurcholismjd10@gmail.com>
2023-10-25 16:58:10 +0700

    implementing pinned repo

--- src/app/home/components/Projects.tsx
+++ src/app/home/components/Projects.tsx
@@ -1,33 +1,36 @@
-import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import { PinnedItem, useGetPinnedItems } from '../../hooks/getPinnedItems';
 
 function Projects() {
-  const arr = useMemo(() => Array.from({ length: 5 }), []);
+  const { loading, error, pinnedItems } = useGetPinnedItems();
+
   return (
     <div id="projects" className="md:pt-[88px] md:-mt-24">
       <h2 className="sticky top-0"></h2>
       <div className="pt-8 pb-10 -my-2 group md:py-0">
-        {arr.map((_, idx) => (
-          <article
-            key={idx}
-            className="my-2 transition bg-secondary md:group-hover:opacity-50 md:hover:!opacity-100 md:hover:scale-110"
-          >
-            <Link
-              to="https://google.com"
-              target="_blank"
-              className="p-10 block"
-              rel="noopener noreferrer"
+        {!error &&
+          !loading &&
+          pinnedItems.map((item: PinnedItem) => (
+            <article
+              key={item.id}
+              className="my-2 transition bg-secondary md:group-hover:opacity-50 md:hover:!opacity-100 md:hover:scale-110"
             >
-              <p className="text-xs font-bold tracking-wide uppercase">
-                Typescript
-              </p>
-              <h3 className="mt-2 text-lg font-semibold leading-tight">
-                Somthing
-              </h3>
-              <p className="mt-2">lorem ipsum sir dolot amet</p>
-            </Link>
-          </article>
-        ))}
+              <Link
+                to={item.url}
+                target="_blank"
+                className="p-10 block"
+                rel="noopener noreferrer"
+              >
+                <p className="text-xs font-bold tracking-wide uppercase">
+                  {item.primaryLanguage.name}
+                </p>
+                <h3 className="mt-2 text-lg font-semibold leading-tight">
+                  {item.name}
+                </h3>
+                <p className="mt-2">lorem ipsum sir dolot amet</p>
+              </Link>
+            </article>
+          ))}
       </div>
     </div>
   );
